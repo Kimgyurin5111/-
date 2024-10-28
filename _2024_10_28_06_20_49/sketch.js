e3fb08ea5c77bfad2dd37c62f41fb397c4d4c53a
Kimgyurin5111 <koong5111@naver.com>
2024-10-28 15:22:09 +0900

    mid test (4)

---
+++ _2024_10_28_06_20_49/sketch.js
@@ -0,0 +1,241 @@
+let img;
+//미리 로딩 준비
+function preload() {
+  img = loadImage("jj.png");
+}
+
+function setup() {
+  createCanvas(1280, 720);
+}
+
+function draw() {
+  image(img, 0, 0, width, height);
+  if (mouseIsPressed === true) {
+    console.log(mouseX, mouseY);
+  }
+  //1
+  // beginShape();
+  // fill(0);
+  // noStroke();
+  // vertex(571,345);
+  // vertex(616,335);
+  // vertex(568,316);
+  // endShape(CLOSE)
+
+  //2
+  // beginShape();
+  // fill(20);
+  // noStroke();
+  // vertex(609, 275);
+  // vertex(616, 335);
+  // vertex(568, 316);
+  // endShape(CLOSE);
+
+  //3
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(609,275);
+  // vertex(616,335);
+  // vertex(673,309);
+  // endShape(CLOSE)
+
+  //4
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(609, 275);
+  // vertex(653, 229);
+  // vertex(673, 309);
+  // endShape(CLOSE);
+
+  //5
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(609, 275);
+  // vertex(568, 316);
+  // vertex(547, 255);
+  // endShape(CLOSE);
+
+  //6
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(609, 275);
+  // vertex(591, 209);
+  // vertex(547, 255);
+  // endShape(CLOSE);
+
+  //7
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(609, 275);
+  // vertex(591, 209);
+  // vertex(653, 229);
+  // endShape(CLOSE);
+
+  //8
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(591, 209);
+  // vertex(653, 229);
+  // endShape(CLOSE);
+
+  // 9
+  // beginShape();
+  // fill(50);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(591, 209);
+  // vertex(582, 148);
+  // endShape(CLOSE);
+
+  // 10
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(671, 118);
+  // vertex(582, 148);
+  // endShape(CLOSE);
+
+  //11
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(671, 118);
+  // vertex(739, 126);
+  // endShape(CLOSE);
+
+  //12
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(758, 229);
+  // vertex(739, 126);
+  // endShape(CLOSE);
+
+  //13
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(676, 163);
+  // vertex(758, 229);
+  // vertex(653, 229);
+  // endShape(CLOSE);
+
+  //14
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(653, 229);
+  // vertex(758, 229);
+  // vertex(755, 270);
+  // endShape(CLOSE);
+
+  //15
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(653, 229);
+  // vertex(696, 271);
+  // vertex(755, 270);
+  // endShape(CLOSE);
+
+  //16
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(653, 229);
+  // vertex(696, 271);
+  // vertex(707, 334);
+  // vertex(674, 309);
+  // endShape(CLOSE);
+
+  //17
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(674, 309);
+  // vertex(707, 334);
+  // vertex(645, 359);
+  // endShape(CLOSE);
+
+  //18
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(785, 327);
+  // vertex(696, 271);
+  // vertex(707, 334);
+  // endShape(CLOSE);
+
+  //19
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(785, 327);
+  // vertex(752, 378);
+  // vertex(707, 334);
+  // endShape(CLOSE);
+
+  //20
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(785, 327);
+  // vertex(696, 271);
+  // vertex(755, 270);
+  // endShape(CLOSE);
+
+  //21
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(785, 327);
+  // vertex(817, 265);
+  // vertex(755, 270);
+  // endShape(CLOSE);
+
+  //22
+  // beginShape();
+  // fill(100);
+  // noStroke();
+  // vertex(785, 327);
+  // vertex(817, 265);
+  // vertex(847, 319);
+  // endShape(CLOSE);
+
+  //23
+  // beginShape();
+  // fill(200);
+  // noStroke();
+  // vertex(758, 229);
+  // vertex(817, 265);
+  // vertex(755, 270);
+  // endShape(CLOSE);
+
+  //24
+  // beginShape();
+  // fill(200);
+  // noStroke();
+  // vertex(758, 229);
+  // vertex(817, 265);
+  // vertex(825, 214);
+  // endShape(CLOSE);
+
+  //25
+  // beginShape();
+  // fill(200);
+  // noStroke();
+  // vertex(758, 229);
+  // vertex(739, 126);
+  // vertex(825, 214);
+  // endShape(CLOSE);
+}
